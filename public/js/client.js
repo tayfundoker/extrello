f7917b2f3c18e726cf87e9373f1eee32d37d54f8
Tayfun Döker <tayfundoker@gmail.com>
2018-04-14 13:48:25 +0300

    first try

--- public/js/client.js
+++ public/js/client.js
@@ -5,19 +5,17 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
-  //Start adding handlers for your capabilities here!
-	'card-buttons': function(t, options) {
-		return [{
-			icon: BLACK_ROCKET_ICON,
- 		text: 'Estimate Size',
-	callback: function(t) {
-	return t.popup({
-	 title: "Estimation",
-	url: 'estimate.html',
-	 });
-	}
-	}];
-	},
+    //Start adding handlers for your capabilities here!
+    'card-buttons': function (t, options) {
+        return [{
+            icon: BLACK_ROCKET_ICON,
+            text: 'Start',
+            callback: function (t) {
+                console.log(t);
+                console.log(options);
+            }
+        }];
+    },
 });
 
 Tre
